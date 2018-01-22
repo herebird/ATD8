532194f7150bb9061b3516521c51d683a4e16e0c
herebird <35319775+herebird@users.noreply.github.com>
2018-01-22 12:27:56 +0700

    Update debian8.sh

--- debian8.sh
+++ debian8.sh
@@ -963,15 +963,15 @@ service squid3 restart
 
 
 # Install Webmin
-#cd
-#wget "https://dl.dropboxusercontent.com/s/f5oukvrl6rxxizz/webmin_1.801_all.deb"
-#dpkg --install webmin_1.801_all.deb;
-#apt-get -y -f install;
-#sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
-#rm /root/webmin_1.801_all.deb
-#service webmin restart
-#service vnstat restart
-#apt-get -y --force-yes -f install libxml-parser-perl
+cd
+wget "https://dl.dropboxusercontent.com/s/f5oukvrl6rxxizz/webmin_1.801_all.deb"
+dpkg --install webmin_1.801_all.deb;
+apt-get -y -f install;
+sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
+rm /root/webmin_1.801_all.deb
+service webmin restart
+service vnstat restart
+apt-get -y --force-yes -f install libxml-parser-perl
 
 
 
